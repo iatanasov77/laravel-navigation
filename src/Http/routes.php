fb7804705915fa21b0ccbbbb48747c183fda4924
Ivan I. Atanasov <ia@vankosoft.org>
2018-06-11 17:47:28 +0300

    Fix controller namespace

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'navigation', 'namespace' => 'Modules\Navigation\Http\Controllers'], function()
+Route::group(['middleware' => 'web', 'prefix' => 'navigation', 'namespace' => 'IA\Laravel\Modules\Navigation\Http\Controllers'], function()
 {
     Route::resource( 'menus', 'MenusController', [ 'as' => 'admin.navigation'] );
 });
